56e71920bed09239b923495f60634f8515470786
Richard Chang <Richard.Chang@todo.com>
2018-07-13 16:17:03 -0700

    Add some error checking.

--- batch_download.py
+++ batch_download.py
@@ -1,12 +1,20 @@
-import urllib
+import os
 import re
+import sys
+import urllib
 
 if __name__ == "__main__":
     # specify the save directory
     dl_dir = "/mnt/d/temp/"
+    if( not os.path.isdir(dl_dir) ):
+        print "Bailing: Invalid download directory " + dl_dir 
+        sys.exit(0)
 
     # specify the html file that is being parsed
     index_file = "/mnt/d/temp/index.html"
+    if( not os.path.exists(index_file) ):
+        print "Bailing: " + index_file + " does not exist."
+        sys.exit(0)
 
     # specify the root url where the files are located
     url_base = "http://mirror.centos.org/centos/7/sclo/x86_64/rh/devtoolset-7/"
